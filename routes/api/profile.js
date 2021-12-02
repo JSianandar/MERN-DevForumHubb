b7621a5f217ca3e7d4222a9c8db2cc89b0ac0ee8
Jason Sianandar <sianandarjason@gmail.com>
2021-12-03 00:05:59 +0700

    able to get logged in user's profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
+// for authentication middleware
+const auth = require("../../middleware/auth");
+// importing Profile model
+const Profile = require("../../models/Profile");
+// importing User model
+const User = require("../../models/User");
 
-// @route   GET api/profile
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => res.send("Profile route"));
+// @route   GET api/profile/me
+// @desc    Get current user's profile
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+
+    // if there's no profile
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+
+    res.json(profile);
+  } catch (e) {
+    console.error(e.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
