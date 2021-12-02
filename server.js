a84e378bc0b7427cd1b5111d0d0b12ac9afb72be
Jason Sianandar <sianandarjason@gmail.com>
2021-12-02 16:45:05 +0700

    Defining our routes

--- server.js
+++ server.js
@@ -11,4 +11,14 @@ connectDB();
 //Landing Page route
 app.get("/", (req, res) => res.send("API Running"));
 
+// Defining our Routes
+// User Routes
+app.use("/api/users", require("./routes/api/users"));
+// Auth Routes
+app.use("/api/auth", require("./routes/api/auth"));
+// Profile Routes
+app.use("/api/profile", require("./routes/api/profile"));
+// Posts Routes
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
