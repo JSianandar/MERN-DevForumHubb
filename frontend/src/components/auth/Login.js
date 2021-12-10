5d258ad574e82edeaf5e67d0b8a3a18b68ab024e
Jason Sianandar <sianandarjason@gmail.com>
2021-12-10 17:41:00 +0700

    Setup login and register routes

---
+++ frontend/src/components/auth/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../App.css";
+
+const Login = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
--- frontend/src/components/layout/Landing.js
+++ frontend/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
@@ -16,12 +17,12 @@ const Landing = () => {
             <br />
             <p className="lead">This is a work in progress</p>
             <div className="buttons">
-              <a href="register.html" class="btn btn-primary">
+              <Link to="/register" class="btn btn-primary">
                 Sign Up
-              </a>
-              <a href="login.html" class="btn btn-light">
+              </Link>
+              <Link to="/login" class="btn btn-light">
                 Login
-              </a>
+              </Link>
             </div>
           </div>
         </div>
