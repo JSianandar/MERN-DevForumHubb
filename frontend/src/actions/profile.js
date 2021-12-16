8bada67a085826c6be1ba6141231e1579b7b2755
Jason Sianandar <sianandarjason@gmail.com>
2021-12-16 18:37:05 +0700

    able to create profile in FE

--- frontend/src/actions/profile.js
+++ frontend/src/actions/profile.js
@@ -19,3 +19,45 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+//Create or update Profile
+export const createProfile =
+  (formData, history, edit = false) =>
+  async (dispatch) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const res = await axios.post("/api/profile", formData, config);
+
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      });
+
+      dispatch(
+        setAlert(
+          edit
+            ? "Profile Successfully Updated"
+            : "Profile Successfully Created",
+          "success"
+        )
+      );
+      // If not editing
+      if (!edit) {
+        history.push("/dashboard");
+      }
+    } catch (e) {
+      const errors = e.response.data.errors;
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      }
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: e.response.statusText, status: e.response.status },
+      });
+    }
+  };
--- frontend/src/components/layout/Landing.js
+++ frontend/src/components/layout/Landing.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { is } from "express/lib/request";
 
 const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
