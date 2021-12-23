03a37cb4f113ba59496656e705f2c93c2853cbe0
Jason Sianandar <sianandarjason@gmail.com>
2021-12-23 15:46:58 +0700

    Able to add post

--- frontend/src/actions/post.js
+++ frontend/src/actions/post.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+  GET_POST,
+} from "./types";
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -64,6 +71,31 @@ export const deletePost = (id) => async (dispatch) => {
     });
 
     dispatch(setAlert("Post successfully removed", "success"));
+  } catch (e) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: e.response.statusText, status: e.response.status },
+    });
+  }
+};
+
+// Add Post
+export const addPost = (formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    const res = await axios.post("/api/posts", formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
+
+    dispatch(setAlert("Post successfully created", "success"));
   } catch (e) {
     dispatch({
       type: POST_ERROR,
--- frontend/src/components/posts/PostItem.js
+++ frontend/src/components/posts/PostItem.js
@@ -13,17 +13,17 @@ const PostItem = ({
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => {
   return (
-    <div class="posts">
-      <div class="post bg-white p-1 my-1">
+    <div className="posts">
+      <div className="post bg-white p-1 my-1">
         <div>
-          <Link to="/profiles">
-            <img class="round-img" src={avatar} alt="" />
+          <Link to={`/profile/${user}`}>
+            <img className="round-img" src={avatar} alt="" />
             <h4>{name}</h4>
           </Link>
         </div>
         <div>
-          <p class="my-1">{text}</p>
-          <p class="post-date">
+          <p className="my-1">{text}</p>
+          <p className="post-date">
             Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
           </p>
           <button
@@ -41,19 +41,19 @@ const PostItem = ({
           >
             <i className="fas fa-thumbs-down" />
           </button>
-          <Link to={`/post/${_id}`} class="btn btn-primary">
+          <Link to={`/post/${_id}`} className="btn btn-primary">
             Discussion{" "}
             {comments.length > 0 && (
-              <span class="comment-count">{comments.length}</span>
+              <span className="comment-count">{comments.length}</span>
             )}
           </Link>
           {!auth.loading && user === auth.user._id && (
             <button
               onClick={(e) => deletePost(_id)}
               type="button"
-              class="btn btn-danger"
+              className="btn btn-danger"
             >
-              <i class="fas fa-times"></i>
+              <i className="fas fa-times"></i>
             </button>
           )}
         </div>
