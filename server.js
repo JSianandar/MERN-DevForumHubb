6ee672a1db449d55ae7c65d9c8049e98685226f6
Jason Sianandar <sianandarjason@gmail.com>
2021-12-23 17:26:06 +0700

    Setting up for production

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 // Express server
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5050;
 
 //Connect Database
 connectDB();
 
 //Init middleware
 app.use(express.json({ extended: false }));
-
-//Landing Page route
-app.get("/", (req, res) => res.send("API Running"));
 
 // Defining our Routes
 // User Routes
@@ -23,5 +20,17 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 // Posts Routes
 app.use("/api/posts", require("./routes/api/posts"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
