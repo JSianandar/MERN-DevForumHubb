14b4b7a4150e801ee94148555b064e3b7ffc62f6
Jason Sianandar <sianandarjason@gmail.com>
2021-12-23 15:14:15 +0700

    Able to like and unlike

--- frontend/src/components/posts/PostItem.js
+++ frontend/src/components/posts/PostItem.js
@@ -3,8 +3,11 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 import { connect } from "react-redux";
+import { addLike, removeLike } from "../../actions/post";
 
 const PostItem = ({
+  addLike,
+  removeLike,
   auth,
   post: { _id, text, name, avatar, user, likes, comments, date },
 }) => {
@@ -22,12 +25,20 @@ const PostItem = ({
           <p class="post-date">
             Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
           </p>
-          <button type="button" class="btn btn-light">
-            <i class="fas fa-thumbs-up"></i>{" "}
-            {likes.length > 0 && <span>{likes.length}</span>}
+          <button
+            onClick={() => addLike(_id)}
+            type="button"
+            className="btn btn-light"
+          >
+            <i className="fas fa-thumbs-up" />{" "}
+            <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
           </button>
-          <button type="button" class="btn btn-light">
-            <i class="fas fa-thumbs-down"></i>
+          <button
+            onClick={() => removeLike(_id)}
+            type="button"
+            className="btn btn-light"
+          >
+            <i className="fas fa-thumbs-down" />
           </button>
           <Link to={`/post/${_id}`} class="btn btn-primary">
             Discussion{" "}
@@ -47,6 +58,8 @@ const PostItem = ({
 };
 
 PostItem.propTypes = {
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
 };
@@ -55,4 +68,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem);
